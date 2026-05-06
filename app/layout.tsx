200edc25056ab48f39a3bd9a7a4e24cb3ce82cfe
tomo0982224338-lang <tomo0982224338@gmail.com>
2026-05-06 15:20:20 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="zh-Hant">
+      <body>{children}</body>
+    </html>
+  );
+}
